58b3fa7fa1d445d13edcd8cffe3b41c499a28dfb
Toshiaki Asai <toshi.alternative@gmail.com>
2020-10-01 22:09:06 +0900

    refactoring test

--- test/multi_vm_test.rb
+++ test/multi_vm_test.rb
@@ -23,75 +23,109 @@ describe(Pluggaloid) do
     assert_includes(pluggaloid.Filter.ancestors, Pluggaloid::Filter, 'Filter should subbclass of Pluggaloid::Filter')
   end
 
-  it 'call event in new vm' do
-    vm_a = Pluggaloid.new(Delayer.generate_class(priority: %i[high normal low], default: :normal))
-    vm_b = Pluggaloid.new(Delayer.generate_class(priority: %i[high normal low], default: :normal))
-    foo = bar = false
-    vm_a.Plugin.create(:foo_plugin) do
-      on_foo do
-        foo = true
+  describe 'calls receive on another vm' do
+    before do
+      @vm_a = Pluggaloid.new(Delayer.generate_class(priority: %i[high normal low], default: :normal))
+      @vm_b = Pluggaloid.new(Delayer.generate_class(priority: %i[high normal low], default: :normal))
+      called = @called = Set.new
+      @vm_a.Plugin.create(:foo_plugin) do
+        on_foo do
+          called << :foo_a
+        end
+
+        on_bar do
+          called << :bar_a
+        end
+
+        on_filter_twice do |num|
+          [num * 2]
+        end
+
+        on_filter_twice_a do |num|
+          [num * 2]
+        end
       end
-    end
-    vm_b.Plugin.create(:bar_plugin) do
-      on_bar do
-        bar = true
-      end
-    end
+      @vm_b.Plugin.create(:bar_plugin) do
+        on_foo do
+          called << :foo_b
+        end
 
-    assert_equal(%i[foo_plugin], vm_a.Plugin.instances_name)
-    assert_equal(%i[bar_plugin], vm_b.Plugin.instances_name)
+        on_bar do
+          called << :bar_b
+        end
 
-    eval_all_events(vm_a.Delayer) do
-      vm_a.Plugin.call(:foo)
-      vm_a.Plugin.call(:bar)
-    end
-    assert foo, 'Event foo should be called'
-    refute bar, 'Event bar should not be called'
-
-    foo = bar = false
-    eval_all_events(vm_b.Delayer) do
-      vm_b.Plugin.call(:foo)
-      vm_b.Plugin.call(:bar)
-    end
-    refute foo, 'Event foo should not be called'
-    assert bar, 'Event bar should be called'
-  end
-
-  it 'calls receive on another vm' do
-    vm_a = Pluggaloid.new(Delayer.generate_class(priority: %i[high normal low], default: :normal))
-    vm_b = Pluggaloid.new(Delayer.generate_class(priority: %i[high normal low], default: :normal))
-    foo = bar = false
-    vm_a.Plugin.create(:foo_plugin) do
-      on_foo do
-        foo = true
-      end
-    end
-    vm_b.Plugin.create(:bar_plugin) do
-      on_bar do
-        bar = true
+        on_filter_twice do |num|
+          [num * 2]
+        end
       end
     end
 
-    assert_equal(%i[foo_plugin], vm_a.Plugin.instances_name)
-    assert_equal(%i[bar_plugin], vm_b.Plugin.instances_name)
-
-    vm_a.connect(vm_b)
-
-    eval_all_events(vm_a.Delayer) do
-      vm_a.Plugin.call(:foo)
-      vm_a.Plugin.call(:bar)
+    it 'assigned plugin' do
+      assert_equal(%i[foo_plugin], @vm_a.Plugin.instances_name)
+      assert_equal(%i[bar_plugin], @vm_b.Plugin.instances_name)
     end
-    eval_all_events(vm_b.Delayer)
-    assert foo, 'Event foo should be called'
-    assert bar, 'Event bar should be called'
 
-    foo = bar = false
-    eval_all_events(vm_b.Delayer) do
-      vm_b.Plugin.call(:foo)
-      vm_b.Plugin.call(:bar)
-    end
-    eval_all_events(vm_a.Delayer)
-    assert foo, 'Event foo should be called'
-    assert bar, 'Event foo should be called'
+    describe 'connect' do
+      before do
+        @vm_a.connect(@vm_b)
+      end
+
+      describe 'fire event in one-side' do
+        before do
+          eval_all_events(@vm_a.Delayer) do
+            @vm_a.Plugin.call(:foo)
+            @vm_a.Plugin.call(:bar)
+          end
+        end
+
+        it 'event foo should be call in VM a' do
+          assert @called.include?(:foo_a)
+        end
+
+        it 'event foo shouldn\'t be call in VM b' do
+          refute @called.include?(:foo_b)
+        end
+
+        describe 'give tick counterpart' do
+          before do
+            eval_all_events(@vm_b.Delayer)
+          end
+
+          it 'event foo should be call in VM b' do
+            assert @called.include?(:foo_b)
+          end
+        end
+      end
+    end                         # connected
+
+    describe 'not connected' do
+      describe 'fire event in one-side' do
+        before do
+          eval_all_events(@vm_a.Delayer) do
+            @vm_a.Plugin.call(:foo)
+            @vm_a.Plugin.call(:bar)
+          end
+        end
+
+        it 'event foo should be call in VM a' do
+          assert @called.include?(:foo_a)
+        end
+
+        it 'event foo shouldn\'t be call in VM b' do
+          refute @called.include?(:foo_b)
+        end
+
+        describe 'give tick counterpart' do
+          before do
+            eval_all_events(@vm_b.Delayer)
+          end
+
+          it 'event foo shouldn\'t be call in VM b' do
+            refute @called.include?(:foo_b)
+          end
+        end
+      end
+    end                         # not connected
+
   end
 end
